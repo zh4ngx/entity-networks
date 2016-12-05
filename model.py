2359bd5dcaeded392b283e26d63b02951360e71b
Andy Zhang <signupforandy@gmail.com>
2016-12-05 12:36:36 -0800

    Add learning rate cut logic & debugging code to understand why predicted prob for class 0-3 is ~0.16

--- model.py
+++ model.py
@@ -9,7 +9,9 @@ BATCH_SIZE = 10
 TOTAL_SEQ = 100000
 HIDDEN_SIZE = 10
 NUM_BATCH = TOTAL_SEQ // (SEQ_LEN * BATCH_SIZE)
-LEARNING_RATE_START = 3e-4
+LEARNING_RATE_START = 1e-1
+LEARNING_RATE_MIN = 1e-6
+LEARNING_RATE_CUT_EPOCH = 10
 NUM_EPOCH = 100
 random_digits = [random.randint(0, 3) for i in range(TOTAL_SEQ)]
 random_inputs = np.zeros([NUM_BATCH, BATCH_SIZE, SEQ_LEN, 10])
@@ -53,10 +55,14 @@ with tf.variable_scope("RNN"):
     output = tf.reshape(tf.concat(1, outputs), [-1, HIDDEN_SIZE])
     cell_states = tf.reshape(tf.concat(1, states), [-1, HIDDEN_SIZE])
 
+    labels_batched = tf.reshape(target_placeholder, [-1])
+    target_weights = tf.ones([BATCH_SIZE * (SEQ_LEN - 1)])
+
+    softmax_outputs = tf.nn.softmax(output)
     loss = tf.nn.seq2seq.sequence_loss(
         [output],
-        [tf.reshape(target_placeholder, [-1])],
-        [tf.ones([BATCH_SIZE * (SEQ_LEN - 1)])])
+        [labels_batched],
+        [target_weights])
 
 optimizer = tf.train.AdamOptimizer(learning_rate_placeholder)
 train_step = optimizer.minimize(loss)
@@ -65,18 +71,37 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 learning_rate = LEARNING_RATE_START
 
+best_loss = np.inf
+epochs_without_improvement = 0
+
 for epoch_idx in range(NUM_EPOCH):
-    fetch_loss = 0
-    # if epoch_idx % 10 == 0:
-    #     learning_rate /= 2
+    if epochs_without_improvement >= LEARNING_RATE_CUT_EPOCH:
+        learning_rate /= 10
+        print("Cutting learning rate to", learning_rate)
+    if learning_rate <= LEARNING_RATE_MIN:
+        print("Ending training since model is not learning")
+        break
     for batch_idx in range(NUM_BATCH):
         batch_input, batch_output = make_batch(random_inputs[batch_idx], random_outputs[batch_idx])
-        fetch_loss, _ = sess.run(
-            [loss, train_step],
+        fetch_output, fetch_labels, fetch_label_weights, fetch_softmax, fetch_loss, _ = sess.run(
+            [output, labels_batched, target_weights, softmax_outputs, loss, train_step],
             feed_dict={
                 input_placeholder: batch_input,
                 target_placeholder: batch_output,
                 learning_rate_placeholder: learning_rate}
         )
+    if best_loss * 0.9999 < fetch_loss:
+        print("Current loss ", fetch_loss, "was not significantly better than best loss of", best_loss)
+        epochs_without_improvement += 1
+        print("Now at", epochs_without_improvement, "epoch(s) without improvement out of", LEARNING_RATE_CUT_EPOCH)
+    else:
+        best_loss = fetch_loss
+        epochs_without_improvement = 0
+        print("Got new best loss of: ", best_loss)
 
-    print("Epoch:", epoch_idx, "Loss:", fetch_loss, "LR:", learning_rate)
+    if not fetch_loss:
+        raise Exception("You set either NUM_EPOCH or NUM_BATCH to 0")
+    else:
+        print("Epoch:", epoch_idx, "Loss:", fetch_loss, "LR:", learning_rate)
+
+print("Training completed - attained loss", best_loss)
